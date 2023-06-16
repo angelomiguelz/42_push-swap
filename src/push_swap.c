73c122a6223dbaa878df3f4782e4f89381aac3ae
mzarichn <angelo.miguel.of@gmail.com>
2023-06-16 16:06:27 +0100

    can do 2 pb

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mzarichn <mzarichn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 16:32:17 by mzarichn          #+#    #+#             */
-/*   Updated: 2023/06/15 17:12:52 by mzarichn         ###   ########.fr       */
+/*   Updated: 2023/06/16 15:41:54 by mzarichn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,36 @@ void	_pephole(t_node *a)
 		printf("NUMBERS: %li\n", (a)->nbr);
 		(a) = (a)->next;
 	}
+	printf("\n");
 }
 
-void	_sort(t_node **a)
+void	_sort(t_node **a, t_node **b)
 {
-	t_node	*b;
-
-	b = NULL;
-	
+	_pb(a, b);
+	_pb(a, b);
 }
 
 int main(int ac, char **av)
 {
 	t_node *a;
-	//t_node *b;
+	t_node *b;
 
+	a = NULL;
+	b = NULL;
 	a = _createList(ac, av);
+	printf("First Stack A:\n");
+	_pephole(a);
 	if (!_checkSorted(a))
 	{
 		if (nodes_quantity(a) == 2)
 			_sa(&a, 1);
 		else
-			_sort(&a);
+			_sort(&a, &b);
 	}
+	printf("\nAfter Stack A:\n");
 	_pephole(a);
+	printf("After Stack B:\n");
+	_pephole(b);
 	return (1);
 } 
 
